5126ae2f9c9a576f68c18f7bd8a6815b648dc75e
BKcore <thibaut.despoulain@bkcore.com>
2012-11-05 15:32:53 +0100

    Added file

--- sources/importer.py
+++ sources/importer.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-import yaml
+import cv
 from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename", help="File to import", metavar="FILE")
 (options, args) = parser.parse_args()
 
-stream = open(options.filename, 'r')
-print yaml.load(stream)
+cv.NamedWindow("GestureIN", cv.CV_WINDOW_AUTOSIZE)
+imagedata = cv.Load(options.filename)
+cv.ShowImage("GestureIN", imagedata) #Show the image
+cv.WaitKey(0)
