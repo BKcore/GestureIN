a0ad19ee782b658ecde57ad1de2403b8a4cec0f5
Ludovic Henry <ludovichenry.utbm@gmail.com>
2012-11-22 10:09:10 +0100

    Added convexityDetexts

--- sources/detect.py
+++ sources/detect.py
@@ -40,23 +40,25 @@ def drawPolygon(im, points, color):
 
 	cv2.line(im, last, first, color)
 
+def drawPoints(im, points, color, radius = 2):
+	for p in points:
+		cv2.circle(im, p, radius, color)
+
 img_ref 		= loadSample(options.filename)
 imb 			= extractBinary(img_ref)
 imb_contours 	= imb.copy()
 
 contours, _ = cv2.findContours(imb_contours, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnt32f 		= contours[0].astype('float32')
-hull        = cv2.convexHull(cnt32f, returnPoints=False)
-hull32f		= hull.astype('float32')
-#													v Ici ça need du ndarray, mais je trouve pas le cv2.createMemStorage...
-convexity   = cv2.convexityDefects(cnt32f, hull32f, np.asarray(cv2.cv.CreateMemStorage()))
+# cnt32f 		= contours[0].astype('float32')
+hull        = cv2.convexHull(contours[0], returnPoints=False)
+# hull32f		= hull.astype('float32')
+# Ici ca need du ndarray, mais je trouve pas le cv2.createMemStorage...
+defects     = cv2.convexityDefects(contours[0], hull)
 
 
 drawPolygon(imb_contours, [(p[0, 0], p[0, 1]) for p in contours[0]], 255)
 drawPolygon(imb_contours, [(p[0, 0], p[0, 1]) for p in cv2.convexHull(contours[0])], 128)
-
-
-
+drawPoints(imb_contours, [tuple(contours[0][p[0][2]][0]) for p in defects], 128, 3)
 
 cv2.namedWindow("Reference")
 cv2.namedWindow("Debug")
