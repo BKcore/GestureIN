5f22e21a60ce516e991bc290df44aa13cda3a97c
Lauranne Didierjean <lauranne.didierjean@gmail.com>
2012-11-23 15:23:08 +0100

    Added normal bayes classifier example

---
+++ sources/bayesClassifier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import cv
+import cv2
+import numpy as np
+from optparse import OptionParser
+
+
+trainData = np.matrix([[0,1,1,4],[0,1,0,0],[6,2,8,1]]).astype('float32')
+
+responses = np.matrix([0,1,2])
+
+classifier = cv2.NormalBayesClassifier()
+classifier.train(trainData,responses)
+
+sample = np.matrix([6,2,8,0]).astype('float32')
+
+_,result = classifier.predict(sample)
+
+print result
