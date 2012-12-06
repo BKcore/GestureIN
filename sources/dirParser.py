396c598a75a188546857362e4ad614ae1646f030
Lauranne Didierjean <lauranne.didierjean@gmail.com>
2012-12-06 14:31:27 +0100

    Added in dirParser.py code for keyboard event manager. Need python-tk package.

--- sources/dirParser.py
+++ sources/dirParser.py
@@ -5,11 +5,12 @@ import cv2
 import numpy as np
 from optparse import OptionParser
 import os.path
+import Tkinter as tk
+
 
 parser = OptionParser()
 parser.add_option("-d", "--directory", dest="dirname", help="Directory to parse", metavar="DIR")
 (options, args) = parser.parse_args()
-
 
 def listdirectory(path): 
     fichier=[] 
@@ -18,7 +19,20 @@ def listdirectory(path):
             fichier.append(os.path.join(root, i)) 
     return fichier
 
+def onKeyPress(event): 
+    if(event.keysym == 'Right'):
+    	print "right arrow"
+    elif(event.keysym == 'Left'):
+    	print "left arrow"
+    else:
+    	print event.keysym
+
 
 files = listdirectory(options.dirname)
-print files
+#print files
 
+tkroot = tk.Tk()
+tkroot.bind('<KeyPress>',  onKeyPress)             
+tkroot.focus()                                     
+tkroot.title('Click Me')
+tkroot.mainloop()
