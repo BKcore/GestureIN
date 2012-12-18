9be39541704f209943cc0c1d16e49afa838deacd
Ludovic Henry <ludovichenry.utbm@gmail.com>
2012-12-18 18:48:48 +0100

    Update run.sh and created cropper.py

--- samples/haar-training/run.sh
+++ samples/haar-training/run.sh
@@ -1,17 +1,13 @@
 #!/bin/zsh
 
-rm -rf haarcascade/*
-
-ls ./positives/*.png | python description_file_generator.py 1>positives.dat
-ls ./negatives/*.png 1>negatives.dat
+find positives -name '*.png' -exec identify -format 'positives/%f 1 0 0 %w %h' '{}' \; 1>positives.dat
+find negatives -name '*.png' 1>negatives.dat
 
 NB_POSITIVES=`cat positives.dat | wc -l`
 NB_NEGATIVES=`cat negatives.dat | wc -l`
 
 echo "launch createsamples"
-opencv_createsamples -info positives.dat -vec positives.vec -num $NB_POSITIVES
+opencv_createsamples -info positives.dat -vec positives.vec -w 48 -h 48 -num $NB_POSITIVES
 
 echo "\nlaunch traincascade"
-opencv_traincascade -data haarcascade -vec positives.vec -bg negatives.dat -numStages 20 -featureType LBP -numPos $NB_POSITIVES -numNeg $NB_NEGATIVES
-
-
+opencv_traincascade -data haarcascade -vec positives.vec -bg negatives.dat -numStages 20 -w 48 -h 48 -numPos $NB_POSITIVES -numNeg $NB_NEGATIVES
